268cd6687f1128443961536a1d06cd19625dc7de
Manel Clos <manelclos@gmail.com>
2018-12-09 16:58:00 +0100

    add TodoList component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <todo-list title="Python Girona TODO list"></todo-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    TodoList
   }
 }
 </script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="todo-list">
+    <h1>{{ title }}</h1>
+    <ul>
+      <li
+        v-for="todo in todos"
+        :key="todo.description">
+        <div>{{ todo.due_date }}</div>
+        <div
+          class="todo-description"
+          >{{ todo.description }}</div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  props: {
+    title: String
+  },
+  data: function () {
+    return {
+      todos: [
+        {
+          description: 'Preparar taller VueJS',
+          due_date: '2018-12-10',
+        },
+        {
+          description: 'Enviar correu recordatori sobre portàtils, etc.',
+          due_date: '2018-12-12',
+        },
+        {
+          description: 'Taller VueJS - UdG',
+          due_date: '2018-12-13',
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+  width: 600px;
+  margin-left: auto;
+  margin-right: auto;
+}
+li {
+  display: flex;
+  padding: 5px 0;
+}
+.todo-description {
+  margin-left: 15px;
+}
+</style>
